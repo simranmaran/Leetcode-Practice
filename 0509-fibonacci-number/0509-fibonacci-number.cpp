78580d14ca2b0c475d7d4fd7f7cd0847c17a2f56
Simran  Maran <153758596+simranmaran@users.noreply.github.com>
2026-05-14 13:10:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.11%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int fib(int n) 
+    {
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+
+        int prev = 0;
+        int curr = 1;
+
+        for(int i=1;i<=n;i++){
+            int next = prev + curr;
+
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;   
+    }
+};

0211cd896f85c76d5453a77c224be5b68438e5f3
Simran  Maran <153758596+simranmaran@users.noreply.github.com>
2026-05-16 19:06:25 +0530

    Time: 3 ms (27.93%), Space: 96 MB (17.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);
+        vector<int> merged;
+        
+        int i = 0, j = 0;
 
-        int m = nums1.size();
-        int n = nums2.size();
-
-        int low = 0, high = m;
-
-        while(low <= high) {
-            int cut1 = (low + high) / 2;
-            int cut2 = (m + n + 1) / 2 - cut1;
-
-            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-
-            int r1 = (cut1 == m) ? INT_MAX : nums1[cut1];
-            int r2 = (cut2 == n) ? INT_MAX : nums2[cut2];
-
-            if(l1 <= r2 && l2 <= r1) {
-                if((m + n) % 2 == 0)
-                    return (max(l1, l2) + min(r1, r2)) / 2.0;
-                else
-                    return max(l1, l2);
-            }
-            else if(l1 > r2) {
-                high = cut1 - 1;
-            }
-            else {
-                low = cut1 + 1;
+        // merge arrays
+        while(i < nums1.size() && j < nums2.size()) {
+            if(nums1[i] < nums2[j]) {
+                merged.push_back(nums1[i]);
+                i++;
+            } else {
+                merged.push_back(nums2[j]);
+                j++;
             }
         }
 
-        return 0.0;
+        // remaining elements
+        while(i < nums1.size()) {
+            merged.push_back(nums1[i]);
+            i++;
+        }
+
+        while(j < nums2.size()) {
+            merged.push_back(nums2[j]);
+            j++;
+        }
+
+        int n = merged.size();
+
+        // find median
+        if(n % 2 == 1) {
+            return merged[n/2];
+        } else {
+            return (merged[n/2] + merged[n/2 - 1]) / 2.0;
+        }
     }
 };

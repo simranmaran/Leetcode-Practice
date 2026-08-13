eadca74f8fc12d0787cadad24a424eeafcfb81b4
Simran  Maran <153758596+simranmaran@users.noreply.github.com>
2026-08-13 21:19:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.47%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+         string ans="";
+        int cnt=0;
+        for(char ch:s)
+        {
+            if(ch==')') cnt--;
+            if(cnt>0)ans+=ch;
+            if(ch=='(') cnt++;
+        }
+        return ans ;
+        
+    }
+};
